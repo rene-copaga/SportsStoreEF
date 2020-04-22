4fce9a91e33da3d84c7b3c8084a1bda5bfbf23d4
Rene Copaga <rene.copaga@gmail.com>
2020-04-22 18:18:50 -0400

    Update

--- SportsStore/Models/DataRepository.cs
+++ SportsStore/Models/DataRepository.cs
@@ -7,17 +7,24 @@ namespace SportsStore.Models
 {
     public class DataRepository : IRepository
     {
-        //private List<Product> data = new List<Product>();
         private DataContext context;
 
         public DataRepository(DataContext ctx) => context = ctx;
 
         public IEnumerable<Product> Products => context.Products.ToArray();
 
+        public Product GetProduct(long key) => context.Products.Find(key);
+
         public void AddProduct(Product product)
         {
             this.context.Products.Add(product);
             this.context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            context.Products.Update(product);
+            context.SaveChanges();
         }
     }
 }
--- SportsStore/Models/IRepository.cs
+++ SportsStore/Models/IRepository.cs
@@ -6,6 +6,10 @@ namespace SportsStore.Models
     {
         IEnumerable<Product> Products { get; }
 
+        Product GetProduct(long key);
+
         void AddProduct(Product product);
+
+        void UpdateProduct(Product product);
     }
 }
