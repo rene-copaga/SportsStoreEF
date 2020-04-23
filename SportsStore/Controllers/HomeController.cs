dd052cf1798ade87fea624494d33890f1eb035c1
Rene Copaga <rene.copaga@gmail.com>
2020-04-22 23:30:24 -0400

    Preparing

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -11,39 +11,26 @@ namespace SportsStore.Controllers
 
         public IActionResult Index()
         {
-            //System.Console.Clear();
             return View(repository.Products);
-        }
-
-        [HttpPost]
-        public IActionResult AddProduct(Product product)
-        {
-            repository.AddProduct(product);
-            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult UpdateProduct(long key)
         {
-            return View(repository.GetProduct(key));
+            return View(key == 0 ? new Product() : repository.GetProduct(key));
         }
 
         [HttpPost]
         public IActionResult UpdateProduct(Product product)
         {
-            repository.UpdateProduct(product);
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult UpdateAll()
-        {
-            ViewBag.UpdateAll = true;
-            return View(nameof(Index), repository.Products);
-        }
-
-        [HttpPost]
-        public IActionResult UpdateAll(Product[] products)
-        {
-            repository.UpdateAll(products);
+            if (product.Id == 0)
+            {
+                repository.AddProduct(product);
+            }
+            else
+            {
+                repository.UpdateProduct(product);
+            }
+            
             return RedirectToAction(nameof(Index));
         }
 
