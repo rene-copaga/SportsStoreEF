3f7e5fbe2f07e3e4b86d1936a6131dead252ffef
Rene Copaga <rene.copaga@gmail.com>
2020-04-24 17:04:26 -0400

    Adding Support for Orders

--- SportsStore/Models/DataContext.cs
+++ SportsStore/Models/DataContext.cs
@@ -9,5 +9,8 @@ namespace SportsStore.Models
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
     }
 }
