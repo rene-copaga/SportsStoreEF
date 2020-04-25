921481420c6f2643dc00e4dfff9b528f65681353
Rene Copaga <rene.copaga@gmail.com>
2020-04-25 15:56:16 -0400

    Adding Search and Ordering Support

--- SportsStore/Models/Pages/QueryOptions.cs
+++ SportsStore/Models/Pages/QueryOptions.cs
@@ -4,5 +4,11 @@
     {
         public int CurrentPage { get; set; } = 1;
         public int PageSize { get; set; } = 10;
+
+        public string OrderPropertyName { get; set; }
+        public bool DescendingOrder { get; set; }
+
+        public string SearchPropertyName { get; set; }
+        public string SearchTerm { get; set; }
     }
 }
